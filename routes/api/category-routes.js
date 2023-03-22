4c5eddbd0ade4d3ffd4fe0fabfc19aa03379fd05
Jacob Nollen <jacob.nollen@netplanner.com>
2023-03-21 23:23:27 -0400

    finished adding all the routes i think, time to test

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,70 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: {
+      model: Product,
+    },
+  }).then((items) => {
+    res.status(200).json(items);
+  }).catch((err) => {
+    res.status(400).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+      },
+    ],
+  }).then((item) => {
+    res.status(200).json(item);
+  }).catch((err) => {
+    res.status(400).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+    .then((item) => {
+      res.status(200).json(item);
+    })
+    .catch(err => {
+      res.status(400).json(err)
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where:{
+      id:req.params.id
+    }
+  }).then((item)=>{
+    res.status(200).json(item);
+  }).catch((err)=>{
+    res.status(400).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    }
+  }).then((count) => {
+    res.status(200).json(JSON.parse(count));
+  }).catch((err) => {
+    res.status(400).json(err);
+  })
 });
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,47 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+      }, {
+        model: ProductTag,
+        include: {
+          model: Tag
+        }
+      }
+    ],
+  }).then(item => {
+    res.status(200).json(item);
+  }).catch((err)=>{
+    res.status(400).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    include: [
+      {
+        model: Category,
+      }, {
+        model: ProductTag,
+        include: {
+          model: Tag
+        }
+      }
+    ],
+    where: {
+      id: req.params.id
+    }
+  }).then(item => {
+    res.status(200).json(item);
+  }).catch((err)=>{
+    res.status(400).json(err);
+  });
 });
 
 // create new product
@@ -91,6 +126,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where:{
+      id: req.params.id,
+    }
+  }).then((count)=>{
+    res.status(200).json(JSON.parse(count));
+  }).catch((err)=>{
+    res.status(400).json(err);
+  });
 });
 
 module.exports = router;

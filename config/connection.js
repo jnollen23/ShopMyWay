f54efdd4189bd89f5450c5d3b852444aa4b6c860
Jacob Nollen <jacob.nollen@netplanner.com>
2023-04-07 00:09:00 -0400

    fixing bugs from grader

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,13 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : (process.env.MYSQL_URL)
     ? new Sequelize(process.env.MYSQL_URL)
-    : console.log("please add a database url to the enviornment variable")
+    : (proces.env.DB_NAME && process.env.DB_PASSWORD && process.env.DB_USER)?
+    new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      }}) 
+    : console.log("You need to add environment varialbes for database connection");
 
 module.exports = sequelize;
